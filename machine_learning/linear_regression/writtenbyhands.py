68478f3851654e6c7cdcf1e5f08855cfb3e0eebc
dongchuangyoubai <1213238708@qq.com>
2019-02-21 10:33:14 +0800

    1

--- machine_learning/linear_regression/writtenbyhands.py
+++ machine_learning/linear_regression/writtenbyhands.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 
 x = [round(10 * random.random(), 2) for _ in range(100)]  # m * n
-y = [[round(2 * i + random.uniform(-1, 1), 2)] for i in x]  # m * 1
+y = [[round(2 * i + 10 + random.uniform(-1, 1), 2)] for i in x]  # m * 1
 y = np.array(y)
 x_mat = np.array([[1, i] for i in x])  # m * (n + 1)
 w = np.array([[1], [1]])  # m * 1
@@ -18,3 +18,4 @@ for i in range(epoches):
 plt.scatter(x, y)
 plt.plot(x, [w[1][0] * i + w[0][0] for i in x])
 plt.show()
+print(w)
