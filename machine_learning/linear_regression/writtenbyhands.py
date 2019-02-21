360234a1bc455badb6766e4809893e5cf0c94fdd
dongchuangyoubai <1213238708@qq.com>
2019-02-21 10:15:22 +0800

    1

--- machine_learning/linear_regression/writtenbyhands.py
+++ machine_learning/linear_regression/writtenbyhands.py
@@ -7,12 +7,14 @@ y = [[round(2 * i + random.uniform(-1, 1), 2)] for i in x]  # m * 1
 y = np.array(y)
 x_mat = np.array([[1, i] for i in x])  # m * (n + 1)
 w = np.array([[1], [1]])  # m * 1
-plt.scatter(x, y)
+
 
 epoches = 100000
 learning_rate = 0.001
 for i in range(epoches):
     h = np.dot(x_mat, w)
     w = w - (1 / 100) * learning_rate * np.dot(np.transpose(x_mat), (h - y))
-    print(w)
 
+plt.scatter(x, y)
+plt.plot(x, [w[1][0] * i + w[0][0] for i in x])
+plt.show()
