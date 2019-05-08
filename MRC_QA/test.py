f6a84ac54fbb568512e91c8a0ca055d70eb7f03e
dongchuangyoubai <1213238708@qq.com>
2019-05-08 19:20:54 +0800

    1

--- MRC_QA/test.py
+++ MRC_QA/test.py
@@ -2,13 +2,32 @@ import json
 
 fr = open('train-v2.0.json', 'r')
 
-js = json.loads(fr.readline())
-print(type(js))
+js = json.loads(fr.read())
 print(js["version"])
-# print(js['data'][0])
-print(js['data'][0]['paragraphs'][0])
-print(js['data'][0]['paragraphs'][0]['qas'])
-print(js['data'][0]['paragraphs'][0]['context'])
+print(len(js['data']))
+print(js['data'][1]['paragraphs'][0])
+print(js['data'][1]['paragraphs'][0]['qas'])
+print(js['data'][1]['paragraphs'][0]['context'])
 data = js['data']
+clean_data = []
 for i in data:
+    for j in i['paragraphs']:
+        context = j['context']
+        for k in j['qas']:
+            question = k['question']
+            if k['answers'] == []:
+                break
+            answer_start = k['answers'][0]['answer_start']
+            answer_end = answer_start + len(k['answers'][0]['text'])
+            # print(len(context))
+            # print(answer_start, answer_end)
+            # print(context[answer_start: answer_end])
+            res = context + "\t" + question + "\t" + str(answer_start) + " " + str(answer_end)
+            clean_data.append(res)
+    #         break
+    #     break
+    # break
 
+with open('train_data', 'w', encoding='utf-8') as fw:
+    for i in clean_data:
+        fw.writelines(i + "\n")
